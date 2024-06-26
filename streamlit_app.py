347cb1cbc5751fe43efacc02c2ceb06d81a67bab
kellymt-umpc <kellymt@upmc.edu>
2024-06-26 16:53:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-import Pandas
+import pandas as pd
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
