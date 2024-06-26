3974f77ef6fcfdf7d6244aeb5bd2dab6dd6accb4
kellymt-umpc <kellymt@upmc.edu>
2024-06-26 16:33:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ session = cnx.session()
 # FruityVice section
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -32,6 +30,8 @@ if ingredients_list:
     ingredients_string = '';
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + '; '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """'
