00a256b24cc5fb4d9cfd25be063c89a678573c82
kellymt-umpc <kellymt@upmc.edu>
2024-06-26 16:14:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
-session = get_active_session()
+session = cnx.session()
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
