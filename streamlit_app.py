0422d37ffa5c1782e8089bcf431a8521bd707735
kellymt-umpc <kellymt@upmc.edu>
2024-06-26 16:30:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,7 @@ session = cnx.session()
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_wdth=True)
-fv_df = st.dataframe(data=fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
